6702a3e20136532162ff59b1dad07ca51ff334e2
Jason <jay_knodel@hotmail.com>
2024-05-28 22:14:49 -0500

    Added enemy states
    States:
    patrol
    chase
    attack

--- Assets/Scripts/CombatController.cs
+++ Assets/Scripts/CombatController.cs
@@ -47,7 +47,7 @@ public class CombatController : NetworkBehaviour
         }
     }
 
-    void Attack(int damage)
+    public void Attack(int damage)
     {
         // Detect enemies in range of the attack
         Collider[] hitColliders = new Collider[10];
@@ -65,15 +65,15 @@ public class CombatController : NetworkBehaviour
     [ServerRpc]
     public void AnimateMeleeServer(GameObject player, string meleeType)
     {
+        // Don't try to trigger animation if already triggered
+        if (animator.GetBool("melee") || animator.GetBool("meleeHeavy")) return;
+
         AnimateMelee(player, meleeType);
     }
 
     [ObserversRpc]
     void AnimateMelee(GameObject player, string meleeType)
     {
-        // Don't try to trigger animation if already triggered
-        if (animator.GetBool("melee") || animator.GetBool("meleeHeavy")) return;
-
         player.GetComponent<Animator>().SetTrigger(meleeType);
     }
 
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,9 @@
 using FishNet.Object;
 using FishNet.Object.Synchronizing;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : NetworkBehaviour
 {
@@ -8,7 +11,99 @@ public class Enemy : NetworkBehaviour
 
     int maxHealth = 100;
 
-    [ServerRpc]
+    NavMeshAgent agent;
+    List<Transform> players = new();
+
+    public LayerMask groundMask, playerMask;
+
+    // patrolling
+    Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    // attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    // states
+    public float sightRange, attackRange;
+    bool playerInSightRange, playerInAttackRange;
+
+    public void Awake()
+    {
+        List<GameObject> playersGO = GameObject.FindGameObjectsWithTag("Player").ToList();
+        foreach (var player in playersGO)
+        {
+            players.Add(player.transform);
+        }
+
+        agent = GetComponent<NavMeshAgent>();
+
+        // CHANGE TO GATHER PLAYERS WHEN THEY CONNECT
+    }
+
+    private void Update()
+    {
+        // check for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, playerMask);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, playerMask);
+
+        if (!playerInSightRange && !playerInAttackRange) Patrolling();
+        if (!playerInSightRange && !playerInAttackRange) ChasePlayer(players[0]); // TEMP!
+        if (!playerInSightRange && !playerInAttackRange) AttackPlayer(players[0]); // TEMP!
+    }
+
+    void Patrolling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        // walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+
+    private void SearchWalkPoint()
+    {
+        // calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, groundMask))
+            walkPointSet = true;
+    }
+
+    void ChasePlayer(Transform playerInRange)
+    {
+        agent.SetDestination(playerInRange.position);
+    }
+
+    void AttackPlayer(Transform playerInRange)
+    {
+        // make sure enemy doesn't move
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(playerInRange);
+
+        if (!alreadyAttacked)
+        {
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    [ServerRpc(RequireOwnership = false, RunLocally = true)]
     public void UpdateHealthServer(GameObject enemy, int amountToChange)
     {
         UpdateHealth(enemy, amountToChange);
